0b39f70633c5cb33b52e0736cea5a92374cf1ef0
henri-marchal-accenture <henri.marchal@accenture.com>
2022-11-14 16:04:07 +0100

    inititial commit

---
+++ springcloudimplementation/src/accenture/marchal/henri/springcloudimplementation/Main.java
@@ -0,0 +1,7 @@
+package accenture.marchal.henri.springcloudimplementation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello to Spring Cloud implementation!");
+    }
+}
